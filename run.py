2d08aaafa5ed87777a9e7eb6a81d257d5300a1d6
Alex Yang <alexyang9345@gmail.com>
2019-11-26 20:29:58 -0600

    ignores comments now

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import smurf_grammar
 
 
 def main(debug):
-    parser = ParserPython(smurf_grammar.program, debug=debug)
+    parser = ParserPython(smurf_grammar.program, smurf_grammar.comment, debug=debug)
 
     f = open("test_cases/test.smu", "r")
     contents = f.read()
